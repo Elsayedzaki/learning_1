94f09e3a2df73d22947bca35f7c58e41187f6ca2
Elsayed Zaki <119413950+Elsayedzaki@users.noreply.github.com>
2024-02-09 00:24:30 +0200

    first commit

---
+++ first_code.c
@@ -0,0 +1,64 @@
+/**********************************************************************************************************
+* @file: first_code.c.
+* @author: Elsayed Zaki.
+* @brief: It is the task for fascinating bionic form Elsayed and I'll do my best to learn more.
+***********************************************************************************************************/
+/************************* Include section ***************************************/
+#include <stdio.h>
+/**********************************************************************************/
+
+/************************* variable section ***************************************/
+char var_1 = 1;
+unsigned char var_2 = 2;
+signed char var_3 = 3;
+int var_4 = 4; // signed default
+unsigned int var_5 = 5;
+short int var_6 = 6;// signed default
+unsigned short var_7 = 7;
+long var_8 = 8;
+long int var_9 = 9;
+unsigned long  var_10 = 10;
+long long var_11 = 11;
+unsigned long long  var_12 = 12;
+unsigned long long int  var_13 = 13;
+/**********************************************************************************/
+
+
+int main(){
+    printf("var result: %d\n", var_1);
+    printf("var result: %d\n", var_2);
+    printf("var result: %d\n", var_3);
+    printf("var result: %d\n", var_4);
+    printf("var result: %d\n", var_5);
+    printf("var result: %d\n", var_6);
+    printf("var result: %d\n", var_7);
+    printf("var result: %d\n", var_8);
+    printf("var result: %d\n", var_9);
+    printf("var result: %d\n", var_10);
+    printf("var result: %d\n", var_11);
+    printf("var result: %d\n", var_12);
+    printf("var result: %d\n", var_13);
+    printf("=====================================\n");
+    printf("size of char: %d\n", sizeof(char));
+    printf("size of char: %d\n", sizeof(unsigned char));
+    printf("size of char: %d\n", sizeof(signed char));
+    printf("size of char: %d\n", sizeof(int));
+    printf("size of char: %d\n", sizeof(unsigned int));
+    printf("size of char: %d\n", sizeof(short int));
+    printf("size of char: %d\n", sizeof(unsigned short));
+    printf("size of char: %d\n", sizeof(long));
+    printf("size of char: %d\n", sizeof(long int));
+    printf("size of char: %d\n", sizeof(unsigned long));
+    printf("size of char: %d\n", sizeof(long long));
+    printf("size of char: %d\n", sizeof(unsigned long long));
+    printf("size of char: %d\n", sizeof(unsigned long long int));
+    return 0;
+}
+
+
+/* 
+***********************************************************************
+User                Data                brief
+***********************************************************************
+Elsayed zaki        faruary 2th         task_1 Added
+*/
